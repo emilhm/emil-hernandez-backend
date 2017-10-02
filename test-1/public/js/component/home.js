202186c9061fe7c74c751d6d09c63e0eb6ea79fb
Emil Hernandez <emilhernandez92@gmail.com>
2017-10-01 22:17:17 -0400

    added react

---
+++ test-1/public/js/component/home.js
@@ -0,0 +1,15 @@
+import React, {Component} from 'react'
+import PropTypes from 'prop-types'
+
+export default class Home extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (<div>Home</div>);
+  }
+}
+
+Home.propTypes = {
+};
