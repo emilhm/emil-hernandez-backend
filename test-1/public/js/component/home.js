674f370a23ece3bf3c609382655b394ffedc2569
Emil Hernandez <emilhernandez92@gmail.com>
2017-10-02 09:53:38 -0400

    WIP - front

--- test-1/public/js/component/home.js
+++ test-1/public/js/component/home.js
@@ -8,10 +8,45 @@ export default class Home extends React.Component {
   }
 
   render() {
-    return (<div>
-      Home
-      <input />
-    </div>);
+    return (
+      <div className="container">
+        Cantida de test cases <input  type="number" min="1" class="form-control" aria-label="Text input with dropdown button" />
+        <div className="container content-testcase">
+          Test 1
+          <div>
+            <div>
+            tamaño de Matrix <input  type="number" min="1" class="form-control" aria-label="Text input with dropdown button" />
+            </div>
+            <div>
+            cantidad de operaciones <input  type="number" min="1" class="form-control" aria-label="Text input with dropdown button" />
+            </div>
+            <div>
+              <div class="row">
+                <div class="col-lg-6">
+                  <div class="input-group">
+                    <div class="input-group-btn">
+                      <button type="button" class="btn btn-secondary dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                        Seleccione una operacion
+                      </button>
+                      <div class="dropdown-menu">
+                        <a class="dropdown-item">Query</a>
+                        <a class="dropdown-item">Update</a>
+                      </div>
+                    </div>
+                    <input type="number" min="1" class="form-control" aria-label="Text input with dropdown button" />
+                    <input type="number" min="1" class="form-control" aria-label="Text input with dropdown button" />
+                    <input type="number" min="1" class="form-control" aria-label="Text input with dropdown button" />
+                    <input type="number" min="1" class="form-control" aria-label="Text input with dropdown button" />
+                    <input type="number" min="1" class="form-control" aria-label="Text input with dropdown button" />
+                    <input type="number" min="1" class="form-control" aria-label="Text input with dropdown button" />
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+);
   }
 }
 
